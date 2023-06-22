bb6269bf0ec6a1062bd20c75d21b26f928dfe5e4
Fakhriddin <gayosiddinov98@gmail.com>
2023-06-22 16:41:44 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': '#54669C',
+        'bg-color': '#20135C',
+      }
+    },
+  },
+  plugins: [],
+}
+
